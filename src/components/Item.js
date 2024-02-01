561bb4d97263d10a004a0a80ee63b01b6d2bde85
Arnold-In-Tech <g16a7782@gmail.com>
2024-02-01 04:12:57 +0300

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useState } from "react";
+
 
 function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false);
+
+  function handleClick(event){
+    setCart((inCart) => !inCart);  // When we click it change from outside cart to inside (inCart=true)
+    event.target.textContent = "Remove From Cart"; 
+  }
+
+  const itemClass = inCart ? "in-cart" : "" 
+  // {/* "" means the item is NOT in the cart */}. 
+  // "" goes last because initially our cart is empty (inCart=false)
+  // {/* "in-cart" the item is in the cart */}
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
+
 
 function ShoppingList({ items }) {
+
+  // Initialise a variable to contain a coppy of the array of all shopping items 
+  const itemData = items;	
+
+  // set initial state of selectedCategory to be a string of "All" to match the first <option> in dropdown
+  const [selectedCategory, setSelectedCategory] = useState("All") 
+
+  // This is the filter value being set onChange using setSelectedCategory function
+  function handleChange(event){
+    setSelectedCategory(event.target.value)
+  }
+
+  // Filter items based on the selectedCategory
+  const itemsToDisplay = itemData.filter((item) => {	
+    if (selectedCategory === "All") {				          // selectedCategory is the value of <select> .i.e. event.target.value 
+      return true;
+    } else {
+      return item.category === selectedCategory;			// With filter, for all elements that return false, don't add them to the new array.
+    }
+  });
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +37,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (  // changed items to the itemsToDisplay
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
